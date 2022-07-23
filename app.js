2dd5a73f753816d98d529014c67395b64910e94e
Berutorion <eagel163013@gmail.com>
2022-07-23 12:15:20 +0800

    Initalized project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const resList = require("./restaurant.json");
+const port = 3000;
+
+//set view engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+//set static file
+app.use(express.static("public"));
+
+//Listen server
+app.listen(port, () => {
+  console.log("Server is working");
+});
+//Route
+app.get("/", (req, res) => {
+  res.render("restaurantList");
+  //res.render("hello");
+});
